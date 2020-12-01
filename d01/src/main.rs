2dc8b6bf09f7123511beeb5050d745ad28327a19
Mathieu Lemay <mathieu.lemay@broadsign.com>
2020-12-01 09:02:45 -0500

    Day 01

--- d01/src/main.rs
+++ d01/src/main.rs
@@ -1,5 +1,20 @@
-use aoc_2020::get_input;
+use aoc_2020::get_input_as_int;
+use itertools::Itertools;
 
 fn main() {
-    let input = get_input("d01.txt");
+    let input = get_input_as_int("d01.txt");
+
+    for values in input.iter().combinations(2) {
+        if values.iter().map(|i| *i).sum::<i32>() == 2020 {
+            println!("Part 1: {}", values.into_iter().product::<i32>());
+            break;
+        }
+    }
+
+    for values in input.iter().combinations(3) {
+        if values.iter().map(|i| *i).sum::<i32>() == 2020 {
+            println!("Part 2: {}", values.into_iter().product::<i32>());
+            break;
+        }
+    }
 }
