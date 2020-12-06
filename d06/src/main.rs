ad714e4b1f928b0dcbb710a2a4313c5b06295768
Mathieu Lemay <acidrain1@gmail.com>
2020-12-06 11:25:04 -0500

    Day 06

--- d06/src/main.rs
+++ d06/src/main.rs
@@ -2,9 +2,57 @@ use std::fmt::Display;
 use std::time::Instant;
 
 use aoc_2020::get_input;
+use std::collections::HashSet;
+
+fn parse_groups_1(lines: &[String]) -> Vec<HashSet<char>> {
+    let mut groups: Vec<HashSet<char>> = Vec::new();
+
+    let mut group: HashSet<char> = HashSet::new();
+    for l in lines {
+        if l.len() == 0 {
+            groups.push(group);
+            group = HashSet::new();
+            continue;
+        }
+
+        group.extend(l.chars());
+    }
+
+    groups.push(group);
+
+    groups
+}
+
+fn parse_groups_2(lines: &[String]) -> Vec<HashSet<char>> {
+    let mut groups: Vec<HashSet<char>> = Vec::new();
+    let mut is_new = true;
+
+    let mut group: HashSet<char> = HashSet::new();
+    for l in lines {
+        if l.len() == 0 {
+            groups.push(group);
+            group = HashSet::new();
+            is_new = true;
+            continue;
+        }
+
+        if is_new {
+            is_new = false;
+            group.extend(l.chars());
+        } else {
+            group = group.intersection(&l.chars().collect()).copied().collect();
+        }
+    }
+
+    groups.push(group);
+
+    groups
+}
 
 fn solve(input: &[String]) -> (impl Display, impl Display) {
-    (0, 0)
+    let p1 = parse_groups_1(input).iter().map(|g| g.len()).sum::<usize>();
+    let p2 = parse_groups_2(input).iter().map(|g| g.len()).sum::<usize>();
+    (p1, p2)
 }
 
 fn main() {
@@ -19,4 +67,55 @@ fn main() {
     println!("Part 1: {}", r1);
     println!("Part 2: {}", r2);
     println!("Duration: {:.3}ms", t);
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{parse_groups_1, parse_groups_2};
+
+    #[test]
+    fn test_parse_groups_1() {
+        let input = "abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b";
+
+        let groups = parse_groups_1(&input.split("\n").map(String::from).collect::<Vec<String>>());
+        println!("Groups: {:?}", groups);
+        assert_eq!(11_usize, groups.iter().map(|g| g.len()).sum())
+    }
+
+    #[test]
+    fn test_parse_groups_2() {
+        let input = "abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b";
+
+        let groups = parse_groups_2(&input.split("\n").map(String::from).collect::<Vec<String>>());
+        println!("Groups: {:?}", groups);
+        assert_eq!(6_usize, groups.iter().map(|g| g.len()).sum())
+    }
 }
