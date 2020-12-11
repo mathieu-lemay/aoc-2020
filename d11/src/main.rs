36b030ee9cd4127673b9f877bccaaf0e6b20bbbf
Mathieu Lemay <mathieu.lemay@broadsign.com>
2020-12-11 14:04:29 -0500

    Day 11: Use seat finding strategy

--- d11/src/main.rs
+++ d11/src/main.rs
@@ -1,3 +1,6 @@
+#[macro_use]
+extern crate lazy_static;
+
 use std::collections::HashMap;
 use std::fmt::Display;
 use std::time::Instant;
@@ -9,6 +12,19 @@ enum Seat {
     Floor,
     Empty,
     Taken,
+}
+
+lazy_static! {
+    static ref DIRECTION_VECTORS: Vec<(i32, i32)> = vec![
+        (-1, -1),
+        (-1, 0),
+        (-1, 1),
+        (0, -1),
+        (0, 1),
+        (1, -1),
+        (1, 0),
+        (1, 1),
+    ];
 }
 
 fn parse(input: &[String]) -> Vec<Vec<Seat>> {
@@ -27,73 +43,30 @@ fn parse(input: &[String]) -> Vec<Vec<Seat>> {
         .collect::<Vec<Vec<Seat>>>()
 }
 
-fn part1(plan: &mut Vec<Vec<Seat>>) -> usize {
-    let ajd_indices = vec![
-        (-1, -1),
-        (-1, 0),
-        (-1, 1),
-        (0, -1),
-        (0, 1),
-        (1, -1),
-        (1, 0),
-        (1, 1),
-    ];
+fn get_seat_next_to_position(
+    plan: &Vec<Vec<Seat>>,
+    current_position: (usize, usize),
+    direction: &(i32, i32),
+) -> Option<Seat> {
+    let h = plan.len() as i32;
+    let w = plan[0].len() as i32;
 
-    let h = plan.len();
-    let w = plan[0].len();
+    let x = current_position.0 as i32 + direction.0;
+    let y = current_position.1 as i32 + direction.1;
 
-    loop {
-        let mut changes = HashMap::new();
-
-        for r in 0..h {
-            for c in 0..w {
-                let s = &plan[r][c];
-                if s == &Seat::Floor {
-                    continue;
-                }
-
-                let adj_occupied = ajd_indices
-                    .iter()
-                    .map(|idx| {
-                        let x = r as i32 - idx.0;
-                        let y = c as i32 - idx.1;
-
-                        if x >= 0
-                            && y >= 0
-                            && x < h as i32
-                            && y < w as i32
-                            && &plan[x as usize][y as usize] == &Seat::Taken
-                        {
-                            1
-                        } else {
-                            0
-                        }
-                    })
-                    .sum::<u32>();
-
-                if s == &Seat::Empty && adj_occupied == 0 {
-                    changes.insert((r, c), Seat::Taken);
-                } else if s == &Seat::Taken && adj_occupied >= 4 {
-                    changes.insert((r, c), Seat::Empty);
-                }
-            }
-        }
-
-        if changes.is_empty() {
-            break;
-        }
-
-        for ((x, y), s) in changes {
-            plan[x][y] = s;
-        }
+    if x < 0 || y < 0 || x >= h || y >= w {
+        return None;
     }
 
-    plan.iter()
-        .map(|r| r.iter().filter(|&s| s == &Seat::Taken).count())
-        .sum()
+    let s = plan[x as usize][y as usize];
+    if s != Seat::Floor {
+        Some(s)
+    } else {
+        None
+    }
 }
 
-fn find_seat_in_direction(
+fn get_first_seat_in_direction(
     plan: &Vec<Vec<Seat>>,
     current_position: (usize, usize),
     direction: &(i32, i32),
@@ -119,18 +92,14 @@ fn find_seat_in_direction(
     }
 }
 
-fn part2(plan: &mut Vec<Vec<Seat>>) -> usize {
-    let direction_vectors = vec![
-        (-1, -1),
-        (-1, 0),
-        (-1, 1),
-        (0, -1),
-        (0, 1),
-        (1, -1),
-        (1, 0),
-        (1, 1),
-    ];
-
+fn count_occupied_seats<F>(
+    plan: &mut Vec<Vec<Seat>>,
+    threshold: u32,
+    seat_finding_strategy: F,
+) -> usize
+where
+    F: Fn(&Vec<Vec<Seat>>, (usize, usize), &(i32, i32)) -> Option<Seat>,
+{
     let h = plan.len();
     let w = plan[0].len();
 
@@ -144,17 +113,17 @@ fn part2(plan: &mut Vec<Vec<Seat>>) -> usize {
                     continue;
                 }
 
-                let visible_occupied = direction_vectors
+                let occupied = DIRECTION_VECTORS
                     .iter()
-                    .map(|dir| match find_seat_in_direction(plan, (r, c), dir) {
+                    .map(|dir| match seat_finding_strategy(plan, (r, c), dir) {
                         Some(Seat::Taken) => 1,
                         _ => 0,
                     })
                     .sum::<u32>();
 
-                if s == &Seat::Empty && visible_occupied == 0 {
+                if s == &Seat::Empty && occupied == 0 {
                     changes.insert((r, c), Seat::Taken);
-                } else if s == &Seat::Taken && visible_occupied >= 5 {
+                } else if s == &Seat::Taken && occupied >= threshold {
                     changes.insert((r, c), Seat::Empty);
                 }
             }
@@ -172,6 +141,14 @@ fn part2(plan: &mut Vec<Vec<Seat>>) -> usize {
     plan.iter()
         .map(|r| r.iter().filter(|&s| s == &Seat::Taken).count())
         .sum()
+}
+
+fn part1(plan: &mut Vec<Vec<Seat>>) -> usize {
+    count_occupied_seats(plan, 4, get_seat_next_to_position)
+}
+
+fn part2(plan: &mut Vec<Vec<Seat>>) -> usize {
+    count_occupied_seats(plan, 5, get_first_seat_in_direction)
 }
 
 fn solve(input: &[String]) -> (impl Display, impl Display) {
